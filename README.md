fdf4c527055c1f2264a78c56dbe9ecbe69953e9f
Shubham Ghosal <shubhamghosal@gmail.com>
2021-09-07 16:11:36 +0530

    Initial Project Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jobs_Made_Easy_Portal_CDAC
---
+++ src/main/java/com/app/jme/JmeWebApplication.java
@@ -0,0 +1,13 @@
+package com.app.jme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeWebApplication.class, args);
+	}
+
+}
