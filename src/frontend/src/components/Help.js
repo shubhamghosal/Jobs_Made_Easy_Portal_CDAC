8831f669a55c77a547cec33f4cf01a87c4144de3
Shubham Ghosal <shubhamghosal@gmail.com>
2021-09-27 16:52:31 +0530

    help.js

---
+++ src/frontend/src/components/Help.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Help extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <header className="jumbotron">
+          <h3>This is Help Page...</h3>
+        </header>
+      </div>
+    );
+  }
+}
